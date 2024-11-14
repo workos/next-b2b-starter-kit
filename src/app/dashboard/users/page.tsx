c9b3013f09cd43994f576032483bf9e446a0c05d
Chance Strickland <hi@chance.dev>
2024-11-14 11:26:18 -0800

    remove log and improve fallback page

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text, Heading } from '@radix-ui/themes';
+import { Box, Card, Flex, Heading, Text } from '@radix-ui/themes';
 
 import { UsersManagement, WorkOsWidgets } from '@workos-inc/widgets';
 import { withAuth } from '@workos-inc/authkit-nextjs';
@@ -8,18 +8,23 @@ export default async function Users() {
   const { user, organizationId } = await withAuth({ ensureSignedIn: true });
 
   if (!organizationId) {
-    return <div>No organization found</div>;
+    return (
+      <Flex direction="column" gap="3" width="100%">
+        <Box>
+          <Heading>Users Management</Heading>
+        </Box>
+        <Card>
+          <Text>No organization found</Text>
+        </Card>
+      </Flex>
+    );
   }
-
-  console.log(organizationId);
 
   const authToken = await workos.widgets.getToken({
     organizationId,
     userId: user.id,
     scopes: ['widgets:users-table:manage'],
   });
-
-  console.log(authToken);
 
   return (
     <Flex direction="column" gap="3" width="100%">
